3aac5a411d4ca64752d7e725c2b8812ce5c9f567
Jose Antonio <josean.basauri@gmail.com>
2015-07-15 22:28:34 +0200

    Inicial

--- models/models.js
+++ models/models.js
@@ -1,39 +1,37 @@
-var path = require('path');
 
-//Postgres DATABASE URL = postgres://user:passwd@host:port/database
-//SQLite DATABASE_URL = sqlite://:@:/
-
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);    
+//Postgres DATABASE_URL = postgres://user:/passwd@host:port/database
+//SQlite DATABASE_URL = sqlite://:@/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+//var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
-var user 	= (url[2]||null);
-var pwd 	= (url[3]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
 var protocol = (url[1]||null);
 var dialect = (url[1]||null);
-var port 	= (url[5]||null);
-var host 	= (url[4]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
 var storage = process.env.DATABASE_STORAGE;
 
+//cargar Modelo ORM
+var Sequelize = require ('sequelize');
+
+//Usar BBDD SQlite o Postgres
+
+var sequelize = new Sequelize(DB_name,user,pwd,
+                    { dialect: protocol,
+                      protocol:protocol,
+                      port: port,
+                      host: host,
+                      storage: storage, //solo SQlite(.env)
+                      omitNull: true    //solo Postgress
+                  }
 
 
-//Cargar el modelo ORM
-var Sequelize = require('sequelize');
+                    );
 
-//User BBDD SQLite: stotage es el fichero donde se guardaran los datos
+//Importar la definicion de la tabla Quiz
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
-	
-var sequelize = new Sequelize(DB_name, user, pwd,
-		{	dialect: 	protocol,
-			protocol: 	protocol,
-			port:  		port,
-			host: 		host,
-			storage: 	storage,  //Solo sirve para SQLite (.env)
-			omitNull: 	true	 //Solo sirve para Postgres	
-		});
-
-
-//Importar la definición de la tabla Quiz que estas en quiz.js
-
-var Quiz = sequelize.import((path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz; //Exportamos la fefinición de la tabla quiz
 
