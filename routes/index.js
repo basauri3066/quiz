2701794d440c5e101192de826c910a6f3d184302
Jose Antonio <josean.basauri@gmail.com>
2015-07-17 19:09:54 +0200

    Programa con acceso a tablas de BD

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+
+/*router.get('/quizes/question', quizController.question);
+  router.get('/quizes/answer', quizController.answer);  */
+
+router.get('/quizes', 			   			quizController.index); 
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);  
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);  
+
 router.get('/author', authorController.author);
 module.exports = router;
