51c308d15896a3eed5ee675420bd01674764aed8
Jose Antonio <josean.basauri@gmail.com>
2015-07-19 18:11:00 +0200

    Ejercicio capitulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -8,18 +8,39 @@ exports.load = function(req, res, next, quizId) {
 				function(quiz){
 					if (quiz) {
 						req.quiz = quiz;
-						next();
+						next(); //con esto llama al siguiente middleware
 					} else {next(new Error('No existe quizId=' + quizId));}
 		}
 	).catch(function(error){next(error);});
 };
 
 //GET /quizes
-exports.index = function(req,res){
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes:quizes});
-	})
+//exports.index = function(req,res){
+//	models.Quiz.findAll().then(function(quizes) {
+//		res.render('quizes/index.ejs', {quizes:quizes});
+//	})
+//};
+
+exports.index = function(req,res){	 
+	var filtro = req.query.search;
+	if(filtro) { //Caso de que se ha puesto alguna condición de búsqueda
+		var condicion = ('%' + filtro + '%').replace(/ /g,'%');
+		
+		models.Quiz.findAll({
+			where: ["pregunta like ?", condicion],
+			order: [['pregunta', 'ASC']]}
+			).then(function(quizes) {	
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});			
+			}).catch(function(error) {next(error);});
+		
+	}	  
+	else{ //caso de que no se haya puesto ningún criterio de búsqueda
+			models.Quiz.findAll().then(function(quizes) {
+				res.render('quizes/index', {quizes: quizes, errors: []});
+			}).catch(function(error) {next(error);});
+		}
 };
+
 
 
 //GET /quizes/:id
