3f9f997cf54be7141b5d5447955ab46c665b0209
Jose Antonio <josean.basauri@gmail.com>
2015-07-27 21:12:20 +0200

    Con autentificacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,14 @@
 //GET /alogin --Formulario de login
 
+//MW de autroización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+		if (req.session.user) {
+				next();
+			} else {
+				res.redirect('/login');
+			}		
+};
+
 exports.new = function(req,res){
 	var errors = req.session.errors || {};
 	req.session.errors ={};
